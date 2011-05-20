deac41b853749124ddbfb7ef250022f682fb4f9d
gashev <gashev@462620.no-reply.drupal.org>
2011-05-20 16:42:26 +0300

    Moved days of month report to /reports/day_of_month.php file.

---
+++ reports/days_of_month.php
@@ -0,0 +1,65 @@
+<?php
+
+function visitors_days_of_month_data($header) {
+  $query = db_select('visitors', 'v');
+  $query->addExpression('COUNT(*)', 'count');
+  $query->addExpression(visitors_date_format_sql('visitors_date_time', '%e'), 'day');
+  $query->groupBy('day');
+  visitors_date_filter_sql_condition($query);
+  
+  if (!is_null($header))
+    $query->extend('TableSort')->orderByHeader($header);
+  
+  return $query->execute();
+}
+
+function visitors_days_of_month() {
+  $header = array(
+    array('data' => t('#')),
+    array('data' => t('Day'), 'field' => 'day', 'sort' => 'asc'),
+    array('data' => t('Pages'), 'field' => 'count'),
+  );
+
+  $results = visitors_days_of_month_data($header);
+  $rows    = array();
+  $i       = 0;
+  $count   = 0;
+
+  foreach ($results as $data) {
+    $rows[] = array(
+      ++$i,
+      (int) $data->day,
+      $data->count
+    );
+
+    $count += $data->count;
+  }
+  $output  = visitors_date_filter();
+
+  if ($count > 0) {
+    /* TODO: Add to img width and height. */
+    /* TODO: For alt use t(). */
+    $output .= '<img src="'. url('visitors/days_of_month/graph') .'" alt="Days of month">';
+  }
+  $output .= theme('table', array('header' => $header, 'rows' => $rows));
+
+  return $output;
+}
+
+function graph_visitors_days_of_month() {
+  $results = visitors_days_of_month_data(NULL);
+  $rows = array();
+
+  for ($i = 1; $i <= 31; $i++) {
+    $rows[$i] = 0;
+  }
+
+  foreach ($results as $data) {
+    $rows[(int)$data->day] = (int)$data->count;
+  }
+
+  // build dates series
+  $dates = range(1, 31);
+  visitors_graph($rows, $dates);
+}
+
